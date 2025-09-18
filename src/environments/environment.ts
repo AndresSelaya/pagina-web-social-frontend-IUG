bef3b3950c7ec2c38f944fcd383544e1c2fa5d9a
iamfab <fabian2015hd@gmail.com>
2025-09-17 23:53:06 -0400

    feat: register to event

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,9 +8,9 @@ export const environment = {
 
   //Backend data
   production: false,
-  BACK_END_HOST_PROD: 'https://devpws.cs.umss.edu.bo/api/v1',
-  BACK_END_HOST_DEV: 'https://devpws.cs.umss.edu.bo/api/v1',
-  BACK_END_HOST_DEV_AUTH: 'https://devpws.cs.umss.edu.bo/api/auth',
+  BACK_END_HOST_PROD: 'http://localhost:9090/api/v1',
+  BACK_END_HOST_DEV: 'http://localhost:9090/api/v1',
+  BACK_END_HOST_DEV_AUTH: 'http://localhost:9090/api/auth',
   BACK_END_HOST_IUG: 'https://vps54072.piramide.de/api',
   BACK_END_PROD_IUG: 'http://localhost:8081/api',
   //Facebook data
